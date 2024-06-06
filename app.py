5fdba97a3379b928fe89276d042161956db2dc63
AlanHH28 <alher2803@gmail.com>
2024-06-05 22:56:52 -0600

    index

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('/index.html')
 
 @app.route('/process', methods=['POST'])
 def process():
@@ -17,7 +17,7 @@ def process():
     print(f"Email: {email}")
     print(f"Message: {message}")
 
-    return render_template('success.html', name=name)
+    return render_template('mensaje.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
